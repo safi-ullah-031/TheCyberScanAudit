6993b89716b32496f65fbd030f8a5aa3f7f31aa3
Safi Ullah <safiullah445@outlook.com>
2025-05-22 16:35:21 +0500

    add file to check the firewalls of the system

---
+++ src/scanners/firewall_checker.py
@@ -0,0 +1,13 @@
+import sys
+from src.utils import run_command
+
+class FirewallChecker:
+    def scan(self, platform):
+        if platform == "windows":
+            cmd = "powershell -Command 'Get-NetFirewallProfile | Select-Object Name, Enabled'"
+        elif platform == "linux":
+            cmd = "sudo ufw status"
+        else:
+            return {"firewall": {"status": "unsupported platform"}}
+        out, err, code = run_command(cmd)
+        return {"firewall": {"output": out, "error": err, "returncode": code}}
