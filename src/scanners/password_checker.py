495a8ce4f8816ac65e0eca5ab5622662f31f162a
Safi Ullah <safiullah445@outlook.com>
2025-06-05 13:13:45 +0500

    fix the python version issue

--- src/scanners/password_checker.py
+++ src/scanners/password_checker.py
@@ -9,7 +9,7 @@ class PasswordChecker:
                 "Get-LocalUser | Where-Object { $_.PasswordRequired -eq $false } | Select-Object Name,Enabled\""
             )
         elif platform == "linux":
-            cmd = "awk -F: '($2=="" || $2=="*" || $2=="!") {print $1}' /etc/shadow"
+            cmd = 'awk -F: \'($2=="" || $2=="*" || $2=="!") {print $1}\' /etc/shadow'
         else:
             return {"passwords": {"status": "unsupported platform", "error": None, "details": None, "returncode": 1}}
         out, err, code = run_command(cmd)
